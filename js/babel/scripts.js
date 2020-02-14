1d970e3bf076e435c59b784e7b6a84ecccebe9af
Safiy Cham <csafiy@yahoo.com>
2020-02-13 21:27:06 -0800

    Fix some styling

--- js/babel/scripts.js
+++ js/babel/scripts.js
@@ -18,3 +18,22 @@ function toggleHamburger() {
     x.className = "nav";
   }
 } // #endregion
+// #region navbar sticky
+// sticky the horizontal navbar to the top after scroll to its div
+
+
+window.onscroll = function () {
+  fixedToTop();
+};
+
+var navLinks = document.getElementById("header"); // let navLinksSticky = navLinks.offsetTop;
+// navbar sticks to top on scroll
+
+function fixedToTop() {
+  // if (window.pageYOffset > navLinksSticky) {
+  if (window.pageYOffset > 1) {
+    navLinks.classList.add("header_sticky");
+  } else {
+    navLinks.classList.remove("header_sticky");
+  }
+} // #endregion

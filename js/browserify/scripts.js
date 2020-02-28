bfd2da2b0b250bf853941affe7ffc79650a5fbd0
Safiy Cham <csafiy@yahoo.com>
2020-02-27 16:32:27 -0800

    Modify banner slideshow to use Slick.js

--- js/browserify/scripts.js
+++ js/browserify/scripts.js
@@ -2,24 +2,35 @@
 "use strict";
 
 // #region lory.js slideshow
-document.addEventListener('DOMContentLoaded', function () {
-  var simple = document.querySelector('.js_slider'); // var multiSlidesVideos = document.querySelector('.js_multislides_videos');
-  // var multiSlidesPhotos = document.querySelector('.js_multislides_photos');
-
-  lory(simple, {
-    infinite: 1
-  }); // lory(multiSlidesVideos, {
-  //   infinite: 1,
-  //   slidesToScroll: 1
-  // });
-  // lory(multiSlidesPhotos, {
-  //   infinite: 4,
-  //   slidesToScroll: 4
-  // });
-}); // #endregion
+// document.addEventListener('DOMContentLoaded', function () {
+//   var simple = document.querySelector('.js_slider');
+// var multiSlidesVideos = document.querySelector('.js_multislides_videos');
+// var multiSlidesPhotos = document.querySelector('.js_multislides_photos');
+// lory(simple, {
+//   infinite: 1
+// });
+// lory(multiSlidesVideos, {
+//   infinite: 1,
+//   slidesToScroll: 1
+// });
+// lory(multiSlidesPhotos, {
+//   infinite: 4,
+//   slidesToScroll: 4
+// });
+// });
+// #endregion
 // #region slick carousel
-
 $(document).ready(function () {
+  $('.banner_slick').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'>\n        <svg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 501.5 501.5'>\n          <g>\n            <path fill='red'\n              d='M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z' />\n          </g>\n        </svg>\n      </button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'>\n        <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"50\" height=\"50\" viewBox=\"0 0 501.5 501.5\">\n          <g>\n            <path fill=\"red\"\n              d=\"M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z\" />\n          </g>\n        </svg>\n      </button>"
+  });
   $('.gallery_responsive').slick({
     dots: false,
     infinite: true,
@@ -80,6 +91,14 @@ function fixedToTop() {
     navLinks.classList.remove("header_sticky");
   }
 } // #endregion
+// #region iframe video stops at lightbox close
+// const video = document.getElementById('test_player');
+// const close = document.querySelector('.lightboxClose');
+// close.addEventListener('click', () => {
+//   video.stopVideo();
+//   console.log('clicked');
+// });
+// #endregion
 // get current year
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,52 @@
 // #region lory.js slideshow
-document.addEventListener('DOMContentLoaded', function () {
-  var simple = document.querySelector('.js_slider');
-  // var multiSlidesVideos = document.querySelector('.js_multislides_videos');
-  // var multiSlidesPhotos = document.querySelector('.js_multislides_photos');
+// document.addEventListener('DOMContentLoaded', function () {
+//   var simple = document.querySelector('.js_slider');
+// var multiSlidesVideos = document.querySelector('.js_multislides_videos');
+// var multiSlidesPhotos = document.querySelector('.js_multislides_photos');
 
-  lory(simple, {
-    infinite: 1
-  });
+// lory(simple, {
+//   infinite: 1
+// });
 
-  // lory(multiSlidesVideos, {
-  //   infinite: 1,
-  //   slidesToScroll: 1
-  // });
+// lory(multiSlidesVideos, {
+//   infinite: 1,
+//   slidesToScroll: 1
+// });
 
-  // lory(multiSlidesPhotos, {
-  //   infinite: 4,
-  //   slidesToScroll: 4
-  // });
-});
+// lory(multiSlidesPhotos, {
+//   infinite: 4,
+//   slidesToScroll: 4
+// });
+// });
 // #endregion
 
 // #region slick carousel
 $(document).ready(function () {
+  $('.banner_slick').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: `<button type='button' class='slick-prev pull-left'>
+        <svg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 501.5 501.5'>
+          <g>
+            <path fill='red'
+              d='M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z' />
+          </g>
+        </svg>
+      </button>`,
+    nextArrow: `<button type='button' class='slick-next pull-right'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 501.5 501.5">
+          <g>
+            <path fill="red"
+              d="M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z" />
+          </g>
+        </svg>
+      </button>`
+  });
+
   $('.gallery_responsive').slick({
     dots: false,
     infinite: true,
@@ -98,6 +123,17 @@ function fixedToTop() {
     navLinks.classList.remove("header_sticky");
   }
 }
+// #endregion
+
+
+// #region iframe video stops at lightbox close
+// const video = document.getElementById('test_player');
+// const close = document.querySelector('.lightboxClose');
+
+// close.addEventListener('click', () => {
+//   video.stopVideo();
+//   console.log('clicked');
+// });
 // #endregion
 
 // get current year
